cf5a6dbc92dee209d254326b9e4d8cb00ddbdb62
Mikael Curbelo <mikael25curbelo@gmail.com>
2025-10-14 21:11:15 -0300

    primer commit proyecto vacio

---
+++ src/test/java/uy/edu/ort/da/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.edu.ort.da.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
